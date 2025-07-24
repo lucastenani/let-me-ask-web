e6a908a455eefd14019fd7fc70fe8c9cd828e86a
lucastenani <lucas.tenani3@gmail.com>
2025-07-24 00:38:12 -0300

    feat: update Room route to include room ID

--- src/pages/create-room.tsx
+++ src/pages/create-room.tsx
@@ -1,38 +1,31 @@
 import { useQuery } from '@tanstack/react-query'
 import { Link } from 'react-router-dom'
-import { Button } from '@/components/ui/button'
 
 type GetRoomsAPIResponse = Array<{ id: string; name: string }>
 
 export function CreateRoom() {
-  const { data: rooms, isLoading } = useQuery({
+  const { data, isLoading } = useQuery({
     queryKey: ['get-rooms'],
     queryFn: async () => {
       const response = await fetch('http://localhost:3333/rooms')
-      const data: GetRoomsAPIResponse = await response.json()
-      return data
+      const result: GetRoomsAPIResponse = await response.json()
+      return result
     },
   })
 
   return (
     <div className="flex h-screen flex-col items-center justify-center ">
-      <h1 className="font-bold text-2xl ">Create a New Room</h1>
-
       {isLoading && <p>Loading...</p>}
 
-      {!isLoading && rooms && (
+      {data && (
         <ul>
-          {rooms.map((room) => (
-            <li key={room.id}>{room.name}</li>
+          {data.map(({ id, name }) => (
+            <li key={id}>
+              <Link to={`/room/${id}`}>{name}</Link>
+            </li>
           ))}
         </ul>
       )}
-
-      <Link to="/room">
-        <Button className="cursor-pointer" variant={'link'}>
-          Room
-        </Button>
-      </Link>
     </div>
   )
 }
