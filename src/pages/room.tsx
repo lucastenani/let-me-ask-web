987fe5326331973658dc1d29bab40d57e5f0db7b
lucastenani <lucas.tenani3@gmail.com>
2025-07-24 00:44:12 -0300

    feat: update Room route parameter to roomId and enhance Room component with navigation logic

--- src/pages/room.tsx
+++ src/pages/room.tsx
@@ -1,14 +1,21 @@
-import { Link } from 'react-router-dom'
+import { Link, Navigate, useParams } from 'react-router-dom'
 import { Button } from '@/components/ui/button'
 
 export function Room() {
+  const { roomId } = useParams<{ roomId: string }>()
+
+  if (!roomId) {
+    return <Navigate replace to="/" />
+  }
+
   return (
     <div className="flex h-screen flex-col items-center justify-center ">
       <h1 className="font-bold text-2xl ">Room Details</h1>
+      <h2 className="mt-4 text-lg">{roomId}</h2>
 
       <Link to="/">
         <Button className="cursor-pointer" variant={'link'}>
-          Create Room
+          Back to Rooms
         </Button>
       </Link>
     </div>
