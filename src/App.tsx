34b52ad2a434089ff77e1cba8f37784b845c10cc
lucastenani <lucas.tenani3@gmail.com>
2025-07-24 00:04:25 -0300

    feat: add Button component

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,9 @@
+import { Button } from './components/ui/button'
+
 export function App() {
-  return <h1>Hello, World!</h1>
+  return (
+    <main className="flex min-h-screen items-center justify-center p-4">
+      <Button>Hello, World!</Button>
+    </main>
+  )
 }
